17d32831330248b0b1f37b097c979d54a974f091
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-07-05 13:54:28 +1000

    hardcode openssl path to use the puppet supplied version; release

--- lib/facter/safe_pp_role_linux.rb
+++ lib/facter/safe_pp_role_linux.rb
@@ -4,7 +4,7 @@ Facter.add(:safe_pp_role) do
   setcode do
     # pick out the custom OID for pp_role, convert `:` to `_` and strip the leading `..` from the capture
     Facter::Core::Execution.exec(
-      "openssl x509 -text < #{Puppet.settings[:certdir]}/#{Facter.value(:fqdn).downcase}.pem | awk 'f{print;f=0} /1.3.6.1.4.1.34380.1.1.13/{f=1}'"
+      "/opt/puppetlabs/puppet/bin/openssl x509 -text < #{Puppet.settings[:certdir]}/#{Facter.value(:fqdn).downcase}.pem | awk 'f{print;f=0} /1.3.6.1.4.1.34380.1.1.13/{f=1}'"
     ).strip.gsub(/::/, '/').gsub(/^../, '')
   end
 end
