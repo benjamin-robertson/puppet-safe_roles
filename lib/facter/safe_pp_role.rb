e4348418d7e989f647a31a581b7259412ffb8591
Benjamin Robertson <ben.robertson@puppet.com>
2022-08-03 12:18:27 +1000

    test with dumb dumb

--- lib/facter/safe_pp_role.rb
+++ lib/facter/safe_pp_role.rb
@@ -25,20 +25,13 @@ Facter.add(:safe_pp_role) do
     puts result
     puts result.class
 
+    if result.class != 'String' 
+      puts 'I am a dumb facter'
+      return 'facter is dumb dumb'
+    end
+
     safe_pp_role = result.strip.gsub(/::/, '/').gsub(/^../, '')
     puts safe_pp_role
-
-    # cert_data = Facter::Core::Execution.exec(
-    #     "openssl x509 -text < #{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
-    # ).to_s.split(/\n/)
-
-    # cert_data.each_with_index do |value, index|
-    #   if value =~ /1.3.6.1.4.1.34380.1.1.13/
-    #     safe_pp_role = cert_data[index+1].strip.gsub(/::/, '/').gsub(/^../, '')
-
-    #     break
-    #   end
-    # end
 
     safe_pp_role
   end
