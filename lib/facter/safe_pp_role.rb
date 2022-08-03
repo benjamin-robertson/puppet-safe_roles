ed33fee5ff9684a155211a5409bb832dfb0a9de3
Benjamin Robertson <ben.robertson@puppet.com>
2022-08-03 11:47:31 +1000

    to s

--- lib/facter/safe_pp_role.rb
+++ lib/facter/safe_pp_role.rb
@@ -20,7 +20,7 @@ Facter.add(:safe_pp_role) do
         arr = (element.to_s).split(' ')
         puts 'arr 3 is'
         puts arr[2]
-        puts arr[1,arr.length]
+        puts arr[1,arr.length].to_s
       end
     end
 
