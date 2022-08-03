d6ca85eadd4d395dc0d59371ef27251e7167b5bb
Benjamin Robertson <ben.robertson@puppet.com>
2022-08-03 10:33:42 +1000

    check openssl output

--- lib/facter/safe_pp_role.rb
+++ lib/facter/safe_pp_role.rb
@@ -4,17 +4,25 @@ Facter.add(:safe_pp_role) do
     # pick out the custom OID for pp_role, convert `:` to `_` and strip the leading `..` from the capture
     safe_pp_role = nil
 
-    cert_data = Facter::Core::Execution.exec(
-        "openssl x509 -text < #{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
-    ).to_s.split(/\n/)
+    puts "#{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
 
-    cert_data.each_with_index do |value, index|
-      if value =~ /1.3.6.1.4.1.34380.1.1.13/
-        safe_pp_role = cert_data[index+1].strip.gsub(/::/, '/').gsub(/^../, '')
+    cert_data = File.open "#{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
 
-        break
-      end
-    end
+    certificate = OpenSSL::x509::Certificate.new cert_data
+
+    puts certificate.extensions
+
+    # cert_data = Facter::Core::Execution.exec(
+    #     "openssl x509 -text < #{Puppet.settings[:certdir]}/#{Puppet.settings[:certname].downcase}.pem"
+    # ).to_s.split(/\n/)
+
+    # cert_data.each_with_index do |value, index|
+    #   if value =~ /1.3.6.1.4.1.34380.1.1.13/
+    #     safe_pp_role = cert_data[index+1].strip.gsub(/::/, '/').gsub(/^../, '')
+
+    #     break
+    #   end
+    # end
 
     safe_pp_role
   end
