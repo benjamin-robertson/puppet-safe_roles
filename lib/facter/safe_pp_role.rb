d996871840bea4153b302a41511cd82709ee32fe
ben <benrobertson9876@gmail.com>
2023-04-14 18:18:31 +1000

    puts

--- lib/facter/safe_pp_role.rb
+++ lib/facter/safe_pp_role.rb
@@ -8,7 +8,7 @@ Facter.add(:safe_pp_role) do
       extentions.each do | element |
         if element.oid == 'pp_role' or element.oid == '1.3.6.1.4.1.34380.1.1.13'
           arr = (element.to_s).split(' ')
-          # arr.each  { |str| puts "#{str}" }
+          arr.each  { |str| puts "#{str}" }
           result = arr[2,arr.length].join
           return result
         end
@@ -25,7 +25,7 @@ Facter.add(:safe_pp_role) do
     if result == nil
       safe_pp_role = nil
     else
-      # puts "Result is: #{result}"
+      puts "Result is: #{result}"
       # safe_pp_role = result.strip.gsub(/::/, '/').gsub(/^\./, '').gsub(/^\.\n/, '').gsub(/^../, '')
       safe_pp_role = result.strip.gsub(/::/, '/').gsub(/[^a-zA-Z\d\/_-]/, '')
     end
