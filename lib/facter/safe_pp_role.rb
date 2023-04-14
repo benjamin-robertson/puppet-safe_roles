315ebca506592e97a7ed3a3afb69c362fd130ed3
ben <benrobertson9876@gmail.com>
2023-04-14 17:43:13 +1000

    Fix

--- lib/facter/safe_pp_role.rb
+++ lib/facter/safe_pp_role.rb
@@ -8,7 +8,7 @@ Facter.add(:safe_pp_role) do
       extentions.each do | element |
         if element.oid == 'pp_role' or element.oid == '1.3.6.1.4.1.34380.1.1.13'
           arr = (element.to_s).split(' ')
-          arr.each | str | { puts "#{str}" }
+          arr.each  { |str| puts "#{str}" }
           result = arr[2,arr.length].join
           return result
         end
